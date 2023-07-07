f7969f57ba5f7a5252980208ddbb82f749df75f5
dmitrii52 <dmitrii.bar97@mail.ru>
2023-07-07 18:12:35 +0300

    сделал задачи номер 1 и номер 2

--- main2.js
+++ main2.js
@@ -1,30 +1,29 @@
 // /*задача 2*/
 
-// let x = prompt("введите значение", "");
-// switch(typeof (x)) {
-// 	case "string":
-// 	console.log("x-строка ");
-// 	break;
-// 	case "number":
-// 		console.log("x-число");
-// 		break;
-// 	case "boolean":
-// 		console.log("x-логическое ");
-// 		break;
-// 	default:
-// 		console.log("тип неизвестен ");
-// }
-
-/*===============================================*/
-
 let x = prompt("введите значение", "");
-let b = typeof x;
-console.log(b)
-if (b === string) {
+switch(typeof (x)) {
+	case "string":
 	console.log("x-строка ");
-}else if (b === number) {
-	console.log("x-число ");
-} else {
-	console.log("неизвестный тип данных ");
+	break;
+	case "number":
+		console.log("x-число");
+		break;
+	case "boolean":
+		console.log("x-логическое ");
+		break;
+	default:
+		console.log("тип неизвестен ");
 }
+/*====================================================*/
+
+// задача 1
+let a = +prompt("введите число", "");
+let num = a;
+if (typeof a !== 'number' || isNaN (a)){
+	console.log ('упс, кажется, вы ошиблись');
+  }	if (num % 2){
+			console.log(" чило-нечетное");
+		} else {
+			console.log(" чило-четное");
+		}
 	
