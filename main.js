de8dd4724f185b9d4523d1d5d26705089345fcea
dmitrii52 <dmitrii.bar97@mail.ru>
2023-07-07 15:15:16 +0300

    сделал новую задачу 3

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 // задание 3 готово
-let a = prompt("напишите слово", "");
-let reverse = a.split("").reverse().join("");
-let element = document.querySelector("div");
-element.innerHTML = `ваше слово ${a}: наоборот ${reverse}`;
-alert(`ваше слово ${a} наоборот ${reverse}`);
+// let a = prompt("напишите слово", "");
+// let reverse = a.split("").reverse().join("");
+// let element = document.querySelector("div");
+// element.innerHTML = `ваше слово ${a}: наоборот ${reverse}`;
+// alert(`ваше слово ${a} наоборот ${reverse}`);
 
 /*====================================================================*/
 
+/*задание номер 4*/
+
+let number = Math.round(Math.random()*100+1); // ROUND - округление до целого числа
+console.log(number);
