ac07a8137cc82e610c882b87dc13002e0c5f1314
dmitrii52 <dmitrii.bar97@mail.ru>
2023-07-15 21:44:54 +0300

    сделал задачу номер 2 модуль 11

---
+++ modul11/mains.js
@@ -0,0 +1,20 @@
+ let num1 = +prompt("введите число до 1000", "");
+ if (num1 < 1000){ 
+	function isPrime(num) {
+	for (let i = 2; i < num; i++) {
+	  if (num % i === 0) return false;
+	}
+	return num !== 1;
+  }
+   isPrime(num=num1);
+   console.log(num);
+   console.log(isPrime(num));
+ } else{
+	alert('слишком большое число');
+  } 
+ if(typeof(isPrime(num))  === "true" ){
+	console.log(num + " простое число ");
+ } else {
+	console.log(num + " составное число ");
+ }
+ 
